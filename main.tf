d2be100ef93cdf93bb348f1ac307554ae85f26bb
shashankAnanthoju <134158447+shashankAnanthoju@users.noreply.github.com>
2024-05-15 15:46:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 # Provider Block
 
 provider "azurerm" {
-    version         =   "~> 2.0"
+    version         =   "~> 3.0"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
